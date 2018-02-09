c5156fb1434d347d3baf263892c9e7b7ed4f5d34
belgattitude <belgattitude@gmail.com>
2018-02-09 18:06:56 +0100

    Moved resources

--- tests/unit/Service/JwtServiceTest.php
+++ tests/unit/Service/JwtServiceTest.php
@@ -55,7 +55,6 @@ class JwtServiceTest extends TestCase
 
         // example token from jwt.io (privateKey: secret)
         $jwt_io = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ';
-        //$rs256 = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.EkN-DOsnsuRjRO6BxXemmJDm3HbxrbRzXglbN2S4sOkopdU4IsDxTI8jO19W_A4K8ZPJijNLis4EZsHeY559a4DFOd50_OqgHGuERTqYZyuhtF39yxJPAjUESwxk2J5k_4zM3O-vtd1Ghyo4IbqKKSy6J9mTniYJPenn5-HIirE";
 
         $token1 = $jwtService->parsePlainToken($jwt_io);
 
